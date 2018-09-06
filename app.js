3a9b061c3b1445e700eab6ffbddeeee904e67753
Adam Bouzourene <>
2018-09-05 21:25:40 -0400

    endpoint added for getting sound list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   path = require('path'),
-  app = express();
+  app = express(),
+  fs = require('fs');
 
 //set the port
 app.set('port', 8050);
@@ -8,7 +9,9 @@ app.set('port', 8050);
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('/sounds', (req, res) => {
-  res.send("these are your sounds");
+  fs.readdir('./dist/sounds/villager', (err, items) => {
+    res.send(items);
+  })
 })
 
 // Listen for requests
